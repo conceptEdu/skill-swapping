3e63031f45364e953052fb084e1c0327cc6f392b
conceptEdu <shyammishrahzb1@gmail.com>
2026-03-15 12:17:40 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -32,20 +32,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-          >
-            Login
-          </Link>
-        </div>
-
-        {/* Mobile Menu Icon (Visual Only) */}
-        <div className="md:hidden text-gray-600">
-          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
-          </svg>
-        </div>
-      </div>
-    </nav>
-  );
-};
-
-export default Navbar;
